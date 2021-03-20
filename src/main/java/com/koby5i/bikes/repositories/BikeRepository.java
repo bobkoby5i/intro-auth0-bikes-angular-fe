1a094e5d3ebfaa08b6026a3791aa0a06908c2b7c
koby5i <bobkoby5i@xdeq.com>
2021-03-20 20:02:31 +0100

    refactor remove spring boot be keep angular fe

--- src/main/java/com/koby5i/bikes/repositories/BikeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.koby5i.bikes.repositories;
-
-import com.koby5i.bikes.models.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BikeRepository extends JpaRepository<Bike, Long> {
-
-}
