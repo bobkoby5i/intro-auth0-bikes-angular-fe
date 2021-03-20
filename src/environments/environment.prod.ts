71743f7895969af6688ed85b8c61fd046fe591ef
koby5i <bobkoby5i@xdeq.com>
2021-03-20 22:47:10 +0100

    fix audience: 'http://localhost:8080',  redirectUri: 'https://koby5i-intro-bike-auth0/callback'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrlBe: 'https://koby5i-intro-bike-auth0-spring.herokuapp.com',
   baseUrlFe: 'https://koby5i-intro-bike-auth0-front.herokuapp.com',
-  audience: 'https://koby5i-intro-bike-auth0-spring',
-  redirectUri: 'http://koby5i-intro-bike-auth0/callback'    
+  audience: 'http://localhost:8080',
+  redirectUri: 'https://koby5i-intro-bike-auth0/callback'    
 };
